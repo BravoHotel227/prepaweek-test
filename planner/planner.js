37ee69f1265251e67c1d5e2a764ac364993a33d2
Ben Harrison <bph958@uowmail.edu.au>
2020-05-26 23:26:16 +1000

    planner styling

--- planner/planner.js
+++ planner/planner.js
@@ -3,7 +3,8 @@ const home = document.getElementById('home'),
   logout = document.getElementById('logout'),
   profile = document.getElementById('profile'),
   content = document.getElementById('content'),
-  table = document.getElementById('table');
+  table = document.getElementById('table'),
+  single_meal = document.getElementById('planner-meal');
 
 function showHome() {
   location.href = '../mainPage.html';
@@ -59,78 +60,188 @@ async function getPlanner() {
       return planner;
     });
   console.log(foo);
-  table.innerHTML = `
-          <table>
-            <tr>
-              <th>Category</th>
-              <th>Monday</th>
-              <th>Tuesday</th>
-              <th>Wednesday</th>
-              <th>Thursday</th>
-              <th>Friday</th>
-              <th>Saturday</th>
-              <th>Sunday</th>
-            </tr>
-            <tr>
-              <td class="breakfast">Breakfast</td>
-              <td class="recipe-info" data-recipeID="${foo.breakfastID[0]}">${foo.breakfastName[0]}</td>
-              <td data-recipeID="${foo.breakfastID[1]}">>${foo.breakfastName[1]}</td>
-              <td data-recipeID="${foo.breakfastID[2]}">>${foo.breakfastName[2]}</td>
-              <td data-recipeID="${foo.breakfastID[3]}">>${foo.breakfastName[3]}</td>
-              <td data-recipeID="${foo.breakfastID[4]}">>${foo.breakfastName[4]}</td>
-              <td data-recipeID="${foo.breakfastID[5]}">>${foo.breakfastName[5]}</td>
-              <td data-recipeID="${foo.breakfastID[6]}">>${foo.breakfastName[6]}</td>
-            </tr>
-            <tr>
-            <td class="lunch">Lunch</td>
-            <td data-recipeID="${foo.lunchID[0]}">${foo.lunchName[0]}</td>
-            <td data-recipeID="${foo.lunchID[1]}">${foo.lunchName[1]}</td>
-            <td data-recipeID="${foo.lunchID[2]}">${foo.lunchName[2]}</td>
-            <td data-recipeID="${foo.lunchID[3]}">${foo.lunchName[3]}</td>
-            <td data-recipeID="${foo.lunchID[4]}">${foo.lunchName[4]}</td>
-            <td data-recipeID="${foo.lunchID[5]}">${foo.lunchName[5]}</td>
-            <td data-recipeID="${foo.lunchID[6]}">${foo.lunchName[6]}</td>
-          </tr>
-          <tr>
-                 <td class="dinner">Dinner</td>
-                 <td>Loaded Beef Burgers</td>
-                 <td>Loaded Beef Burgers</td>
-                 <td>Loaded Beef Burgers</td>
-                 <td>Loaded Beef Burgers</td>
-                 <td>Loaded Beef Burgers</td>
-                 <td>Loaded Beef Burgers</td>
-                 <td>Loaded Beef Burgers</td>
-               </tr>
-               <tr>
-               <td class="dessert">Dessert</td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-             </tr>
-             <tr>
-             <td class="snack">Snack</td>
-             <td>Kale Chips</td>
-             <td>Kale Chips</td>
-             <td>Kale Chips</td>
-             <td>Kale Chips</td>
-             <td>Kale Chips</td>
-             <td>Kale Chips</td>
-             <td>Kale Chips</td>
-           </tr>
-          </table`;
+  showPlanner(foo);
 }
 
-function getRecipeById(recipeID) {
-  fetch(`https://www.mealprepapi.com/api/v1/recipes/${recipeID}`)
+function showPlanner(planner) {
+  var tbl = document.createElement('table');
+  var tblBody = document.createElement('tbody');
+  var headerTitle = [
+    'Category',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+  var category = ['Breakfast', 'Lunch', 'Dinner', 'Dessert', 'Snack'];
+  var headRow = document.createElement('tr');
+  for (var i = 0; i < 8; i++) {
+    var header = document.createElement('th');
+    var headertext = document.createTextNode(`${headerTitle[i]}`);
+    header.appendChild(headertext);
+    headRow.appendChild(header);
+  }
+  tbl.appendChild(headRow);
+
+  // creating cells
+  for (var i = 0; i < 5; i++) {
+    var row = document.createElement('tr');
+
+    for (var j = -1; j < 7; j++) {
+      var cell = document.createElement('td');
+      var cellText;
+      switch (i) {
+        case 0: {
+          if (j === -1) {
+            cellText = document.createTextNode(`Breakfast`);
+          } else if (planner.breakfastName[j] == null) {
+            cellText = document.createTextNode('');
+          } else {
+            cellText = document.createTextNode(`${planner.breakfastName[j]}`);
+            cell.setAttribute('data-recipeID', planner.breakfastID[j]);
+          }
+          break;
+        }
+        case 1: {
+          if (j === -1) {
+            cellText = document.createTextNode(`Lunch`);
+          } else if (planner.lunchName[j] == null) {
+            cellText = document.createTextNode('');
+          } else {
+            cellText = document.createTextNode(`${planner.lunchName[j]}`);
+            cell.setAttribute('data-recipeID', planner.lunchID[j]);
+          }
+          break;
+        }
+        case 2: {
+          if (j === -1) {
+            cellText = document.createTextNode(`Dinner`);
+          } else if (planner.dinnerName[j] == null) {
+            cellText = document.createTextNode('');
+          } else {
+            cellText = document.createTextNode(`${planner.dinnerName[j]}`);
+            cell.setAttribute('data-recipeID', planner.dinnerID[j]);
+          }
+          break;
+        }
+        case 3: {
+          if (j === -1) {
+            cellText = document.createTextNode(`Dessert`);
+          } else if (planner.dessertName[j] == null) {
+            cellText = document.createTextNode('');
+          } else {
+            cellText = document.createTextNode(`${planner.dessertName[j]}`);
+            cell.setAttribute('data-recipeID', planner.dessertID[j]);
+          }
+          break;
+        }
+        case 4: {
+          if (j === -1) {
+            cellText = document.createTextNode(`Snack`);
+          } else if (planner.snackName[j] == null) {
+            cellText = document.createTextNode('');
+          } else {
+            cellText = document.createTextNode(`${planner.snackName[j]}`);
+            cell.setAttribute('data-recipeID', planner.snackID[j]);
+          }
+          break;
+        }
+      }
+      if (j !== -1) {
+        cell.classList.add('recipe-info');
+      }
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+    tblBody.appendChild(row);
+  }
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  table.appendChild(tbl);
+  // sets the border attribute of tbl to 2;
+  tbl.setAttribute('border', '2');
+}
+
+async function getRecipeById(recipeID) {
+  const recipe = await fetch(
+    `https://www.mealprepapi.com/api/v1/recipes/${recipeID}`
+  )
     .then((response) => response.json())
     .then((results) => {
-      const recipe = results.data;
-      console.log(recipe);
+      return results.data;
     });
+  showRecipe(recipe);
+  console.log(recipe);
+}
+
+function showRecipe(recipe) {
+  const ingredients = [];
+  for (let i = 0; i < recipe.ingredientNames.length; i++) {
+    if (recipe.ingredientQtys[i] === undefined) {
+      ingredients.push(`
+      ${recipe.ingredientNames[i]}
+    `);
+    } else {
+      ingredients.push(`
+      ${recipe.ingredientNames[i]} - ${recipe.ingredientQtys[i]}
+    `);
+    }
+  }
+  single_meal.innerHTML = `
+    <div class="planner-meal-card">
+      <div class="planner-meal-btn">
+        <button id='edit-recipe' onclick='editRecipe()'>Edit</button>
+        <button id='delete-recipe' onclick='deleteRecipe()'>Delete</button>
+        <button class="close-meal-btn" id="close-meal-btn" onclick="closeMeal()">&times;</button>
+      </div>
+      <div class="planner-meal-title" data-RecipeId=${
+        recipe.id
+      } id="recipe-title">
+      ${recipe.title}
+      </div>
+      <div class="planner-meal-body" id="planner-sgl-meal">
+      <div class="planner-meal-category">
+          ${recipe.category ? `<p>${recipe.category}</p>` : ''}
+      </div>
+      <div class="planner-meal-serves">
+        <h5>Prep time: ${recipe.prepTime} Serves: ${recipe.serves}</h5>
+      </div>
+      <div class="planner-meal-types">
+       ${recipe.vegan ? '<span class="vegan-card">Vegan</span>' : ''}
+       ${
+         recipe.glutenFree ? '<span class="gluten-card">Gluten Free</span>' : ''
+       }
+      </div>
+      <div class="planner-meal-main">
+        <div class="info-block">
+          <div class="planner-meal-ingredients">
+          <h2>Ingregients</h2>
+          <ul>
+            ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+         </ul>
+        </div>
+        <div class="planner-meal-directions">
+          <h2>Directions</h2>
+          <p>${recipe.directions}</p>
+        </div>
+        </div>
+        <div class="planner-meal-notes">
+          <h2 id="note-heading">${recipe.notes ? 'Notes' : ''}</h2>
+           <p>${recipe.notes}</p> 
+        </div>
+      </div>
+  </div>
+`;
+  if (recipe.notes) {
+    document.getElementById('note-heading').style.borderBottom =
+      '1px solid #727070';
+  } else {
+    document.getElementById('note-heading').style.borderBottom = '';
+  }
 }
 
 // Event listeners
